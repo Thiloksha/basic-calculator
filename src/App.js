7b0ccfbb723cf5365c69b329c3e0cfd444a5658f
Thiloksha <thiloksha25@gmail.com>
2025-06-27 16:41:17 +0530

    created basic structure and some styles

--- src/App.js
+++ src/App.js
@@ -2,8 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div></div>
-  );
+    <div className='calculator'>
+      <div className='container'>
+        <dvi className='output'>
+          <div className='previous-operand'> ergwetgwt</div>
+          <div className='current-operand'>ddrtgaer</div>
+        </dvi>
+        <button className='span-two'>AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>×</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span-two'>=</button>
+    </div>
+    </div>
+
+  )
 }
 
 export default App;
