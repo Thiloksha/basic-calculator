60d846df75c1c85f87060e3c4b2c7ab820d51b28
Thiloksha <thiloksha25@gmail.com>
2025-06-27 18:14:09 +0530

    complete styles for calculater

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ function App() {
           <div className='previous-operand'> ergwetgwt</div>
           <div className='current-operand'>ddrtgaer</div>
         </dvi>
-        <button className='span-two'>AC</button>
-        <button>DEL</button>
-        <button>÷</button>
+        <button className='span-two' id='del-ac-btn'>AC</button>
+        <button id='del-ac-btn'>DEL</button>
+        <button className='operand'>÷</button>
         <button>1</button>
         <button>2</button>
         <button>3</button>
-        <button>×</button>
+        <button className='operand'>×</button>
         <button>4</button>
         <button>5</button>
         <button>6</button>
-        <button>+</button>
+        <button className='operand'>+</button>
         <button>7</button>
         <button>8</button>
         <button>9</button>
-        <button>-</button>
+        <button className='operand'>-</button>
         <button>.</button>
         <button>0</button>
         <button className='span-two'>=</button>
